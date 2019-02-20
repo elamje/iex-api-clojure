59146aa6381ffea898aebf15249ca30f565fb132
John Elam <elam.john@pwc.com>
2019-02-20 16:39:31 -0600

    add stock and price objects to project

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject lib "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject io.johnelam/lib "0.1.0"
+  :description "IEX Trading Information API 1.0 | real time stock information | direct from exchange"
+  :url "https://iextrading.com/developer/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [ [org.clojure/clojure "1.10.0"]
+                  [http-kit "2.3.0"]]
+  :main lib.core
   :repl-options {:init-ns lib.core})
--- src/lib/core.clj
+++ src/lib/core.clj
@@ -1,6 +1,18 @@
-(ns lib.core)
+(ns lib.core
+  (:gen-class)
+  (:require [lib.stock])
+  (:require [lib.price :refer :all])
+  (:import  [lib.stock Stock]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn api
+  "Call Api here"
+  []
+  (def stock (Stock. "apple" "AAPL" 12 11 13))
+  (price false (get stock :symbol)))
+
+(defn -main 
+  "call something"
+  [& args]
+  (api))
+
