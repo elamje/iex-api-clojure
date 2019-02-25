56b3668d11d367d7aa96b69373b418a4db0cc103
John Elam <elam.john@pwc.com>
2019-02-25 13:39:18 -0600

    Update getdata.clj with functions from test_data.clj, refactor for unit testing.

--- src/lib/transformations.clj
+++ src/lib/transformations.clj
@@ -3,6 +3,12 @@
   (:require [clojure.data.json :as json]))
 
 (defn json2map
-  [body]
-  (json/read-str body))
+  "take the json from the api and digest to map"
+  [api-response]
+  (json/read-str api-response))
+
+(defn symbols2map
+  "take the lazy seq from building data and digest to map"
+  [data]
+  (def data_reform (into {} data)))
 
