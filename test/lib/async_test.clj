7501a93313b91a9204a235bd961256573f1a98bf
John Elam <j3elam@utexas.edu>
2019-02-28 17:17:55 -0600

    Not sure what the diff is

--- test/lib/async_test.clj
+++ test/lib/async_test.clj
@@ -2,11 +2,17 @@
   (:require [clojure.test :refer :all])
   (:require [lib.async :refer :all]))
 
-(deftest async-price-test
-  (testing "Get All nasdaq prices Async"
-    (is
+(deftest async-data-test
+  (testing "Get random subset NASDAQ Async info and prices"
+    (is ;Info/Stats
       (let [syms (list 'aapl 'goog)]
         (-> syms
-          (async-prices)
+          (async-data "stats")
+          (nil?)
+          (not))))
+    (is ;Price
+      (let [syms (list 'aapl 'goog)]
+        (-> syms  
+          (async-data "price")
           (nil?)
           (not))))))
